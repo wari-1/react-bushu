0b6ee394dc6c83ff85e860f3330b1cbca8a80390
wari-1 <405770221@qq.com>
2019-08-19 15:50:24 +0800

    react

---
+++ precache-manifest.93b69727033d051d066a2f273a0901c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4721ff0982d7de273134ffd699111f1",
+    "url": "/react-bushu/index.html"
+  },
+  {
+    "revision": "a61dd4bc3838b9baa444",
+    "url": "/react-bushu/static/css/main.3a7a1e40.chunk.css"
+  },
+  {
+    "revision": "0681e5bb692bcc9416aa",
+    "url": "/react-bushu/static/js/2.ed64165c.chunk.js"
+  },
+  {
+    "revision": "a61dd4bc3838b9baa444",
+    "url": "/react-bushu/static/js/main.042f538e.chunk.js"
+  },
+  {
+    "revision": "b1ea01805cda07faae84",
+    "url": "/react-bushu/static/js/runtime~main.d25e4b4b.js"
+  }
+]);
--- src/component/About.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class About extends Component {
-  state = {};
-  render() {
-    return <div>about</div>;
-  }
-}
-
-export default About;
